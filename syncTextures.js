9004dedd006217669bc602a93caf41175d341537
Snowy Peak <contact@snowypeaksoftware.com>
2022-10-29 09:59:46 -0600

    initial documentation

--- syncTextures.js
+++ syncTextures.js
@@ -1,13 +1,25 @@
-// syncs textures from the server to local development environment
+/** syncs textures from the server to local development environment
+ *  
+ *  
+ */
 
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
 const lodash = require('lodash/array');
-// server-side script with updated image list, output in json
-const url = 'https://www.project-atlantis.com/wp-content/threejs-simulation/textures/getTextures.php';
+// url to server-side script with updated image list (json)
+const textureList = 'https://www.project-atlantis.com/wp-content/threejs-simulation/textures/getTextures.php';
+// url to location of remote texture files
 const textureSource = 'https://www.project-atlantis.com/wp-content/threejs-simulation/textures';
 
+/**
+ * scans local textures
+ *
+ * @param {string}  localPath   // path to local textures
+ * @param {array}   fileArray   // callback to merge textures into single array
+ *
+ * @return {array}  fileArray   // list of local textures
+ */
 function getLocalTextures(localPath, fileArray) {
   files = fs.readdirSync(localPath);
   fileArray = fileArray || []
@@ -27,7 +39,15 @@ function getLocalTextures(localPath, fileArray) {
   return fileArray;
 }
 
-async function getRemoteTextures() {
+/**
+ * pulls list (json) of remote textures from the server
+ *
+ * @param {string}  url             // url to json output
+ *
+ * @return {array}  remoteTextures  // list of remote textures
+ */
+async function getRemoteTextures(url) {
+  let url = url || textureList;
   return new Promise ((resolve, reject) => {
     let req = https.get(url,(res) => {
       let body = "";
@@ -53,25 +73,14 @@ async function getRemoteTextures() {
   });
 }
 
-
-async function syncTextures() {
-  const remoteTextures = await getRemoteTextures();
-  const localTextures = getLocalTextures('./textures');
-  const localTexturePath = path.join(__dirname, 'textures', '/');
-  const diff = lodash.difference(remoteTextures, localTextures);
-
-  await Promise.all(diff.map( async file => {
-    let localFile = path.join(localTexturePath, file);
-    let remoteFile = textureSource + file;
-    let finishedFile = await downloadFile(remoteFile, localFile, localTexturePath);
-    console.log('Downloaded ' + finishedFile);
-    Promise.resolve(finishedFile);
-  }));
-
-  console.log('Files Synced.');
-  return Promise.resolve('synced');
-}
-
+/**
+ * downloads the file from the server
+ *
+ * @param {string}  remoteFile  // url to remote file location
+ * @param {string}  localFile   // local filename with full path
+ *
+ * @return {string} remoteFile  // downloaded filename
+ */
 async function downloadFile(remoteFile, localFile) {
   return new Promise ((resolve, reject) => {
     let req = https.get(remoteFile, (res) => {
@@ -92,5 +101,28 @@ async function downloadFile(remoteFile, localFile) {
   });
 }
 
-syncTextures()
+/**
+ * determines missing files and iterates them through downloadFile
+ *
+ * @return {string}   
+ */
+async function syncTextures() {
+  const remoteTextures = await getRemoteTextures();
+  const localTextures = getLocalTextures('./textures');
+  const localTexturePath = path.join(__dirname, 'textures', '/');
+  const diff = lodash.difference(remoteTextures, localTextures);
 
+  await Promise.all(diff.map( async file => {
+    let localFile = path.join(localTexturePath, file);
+    let remoteFile = textureSource + file;
+    let finishedFile = await downloadFile(remoteFile, localFile, localTexturePath);
+    console.log('Downloaded ' + finishedFile);
+    Promise.resolve(finishedFile);
+  }));
+
+  console.log('Files Synced.');
+  return Promise.resolve('synced');
+}
+
+
+syncTextures();
